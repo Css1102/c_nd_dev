e23abb5a8c1850be359000ec5d9981fd18c05f10
Css1102 <sethischiranjeev@gmail.com>
2025-06-07 12:28:57 +0530

    second c commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express=require("express")
 const app=express()
 
-app.use('/home',(req,res)=>{
-res.send("This is called from the home method")
+/*Route matching is handled through regex expression so the order of different route calls 
+matters a lot. The route that is matched first will be displayed on the server.*/
+// will match only the get call to '/test'
+app.get('/test',(req,res)=>{
+res.send("This is my test in the course called from get")
 })
+// will match post call to the /test
+app.post('/test',(req,res)=>{
+res.send("This is my test in the course from the post")
+})
+
+// will match all http calls to /test
 app.use('/test',(req,res)=>{
 res.send("This is my test in the course")
 })
