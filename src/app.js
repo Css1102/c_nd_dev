25e5e3d059d7d31062449dc3547e566749e5ad0b
Css1102 <sethischiranjeev@gmail.com>
2025-06-07 13:09:48 +0530

    third c commit

--- src/app.js
+++ src/app.js
@@ -7,6 +7,22 @@ matters a lot. The route that is matched first will be displayed on the server.*
 app.get('/test',(req,res)=>{
 res.send("This is my test in the course called from get")
 })
+// ${req.params[password]}
+// In this request we have made a dynamic route which matches the request with given id password and 
+// name and it is stored in the req.params as an object and can be referenced from there
+app.get('/user/:userId/:password/:name',(req,res)=>{
+console.log(req.params)
+res.send(`recieved a get request of user with id ${req.params?.userId}  and password ${req.params?.password}`)
+}
+)
+// in this request if an optional parameter is passed it is stored in req.query as an object and can be
+// consoled from there.
+app.get('/users',(req,res)=>{
+console.log(req.query)
+res.send('recieved a get request of users')
+}
+)
+
 // will match post call to the /test
 app.post('/test',(req,res)=>{
 res.send("This is my test in the course from the post")
