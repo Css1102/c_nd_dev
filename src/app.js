67d322abed6a32766ecc0fedcbb47191916dfb17
Css1102 <sethischiranjeev@gmail.com>
2025-06-14 12:08:13 +0530

    7th c commit user and feed get calls

--- src/app.js
+++ src/app.js
@@ -2,23 +2,45 @@ const express=require("express")
 const app=express()
 const {DBConnect}=require('./config/database.js')
 const {userModel}=require('./model/user.js')
-
-
+// middleware used to convert the json data from request into js object and pushing into db
+app.use(express.json())
 app.post('/signup',async(req,res)=>{
-const userObj={
-firstName:"Akshay",
-lastName:"Saini",
-email:"akshaysaini@gmail.com",
-age:28,
-password:"akshay@123"
-}
-// creating instance of the userSchema and pushing the data into the collection.
-const user=new userModel(userObj)
+console.log(req)
+// creating instance of the user model and pushing the data into the collection.
+const user=new userModel(req.body)
 await user.save()
 res.send("Data sent to db")
 })
-// first we need to connect to the database only then we should listen to the server so app.listen is 
-// nested inside the then block of dbconnect function
+app.get('/user',async(req,res)=>{
+const userEmail=req.body?.email
+try{
+const user=await userModel.findOne({email:userEmail})
+if(!user){
+res.status(400).send("user not found")
+}
+else{
+res.send(user)
+}
+}
+catch(err){
+console.log("oops some issue is there")
+}
+})
+
+app.get('/feed',async(req,res)=>{
+try{
+const users=await userModel.find({})
+if(users.length===0){
+res.status(401).send("Something went wrong")
+}
+else{
+res.send(users)
+}
+}
+catch(e){
+res.status(404).send("user not found")
+}
+})
 DBConnect().then(()=>{
 console.log("database connection estabished")
 app.listen(7646,()=>{
