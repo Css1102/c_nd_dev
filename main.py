e2079dd7e5485681c4beb108f01f03a13857cb32
Css1102 <sethischiranjeev@gmail.com>
2025-08-07 08:49:30 +0530

    Port commit

--- main.py
+++ main.py
@@ -61,4 +61,5 @@ def match_from_db(payload:UserIDs):
 
 if __name__ == "__main__":
     import uvicorn  
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
