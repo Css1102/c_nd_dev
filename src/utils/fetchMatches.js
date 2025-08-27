23f24f4740ca4b3b31c28e4e58d998fb1291562d
Chiranjeev Singh Sethi <chiranjeev@Chiranjeevs-MacBook-Air.local>
2025-08-27 13:34:43 +0530

    fresh backend commit

--- src/utils/fetchMatches.js
+++ src/utils/fetchMatches.js
@@ -38,7 +38,7 @@ async function fetchSkillMatches(userIds) {
   }
   try {
     console.log(userIds)
-    const response = await axios.post(`${FASTAPI_URL}match_from_db`, userIds, {
+    const response = await axios.post(`${FASTAPI_URL}/match_from_db`, userIds, {
       headers: { "Content-Type": "application/json" },
     });
       console.log("FastAPI response:", response.data);
