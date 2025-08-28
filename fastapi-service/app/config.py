00cb155acbb35231720dcdd0e8aeb44f10664427
Chiranjeev Singh Sethi <chiranjeev@Chiranjeevs-MacBook-Air.local>
2025-08-28 19:19:49 +0530

    fastapi error commit_8

--- fastapi-service/app/config.py
+++ fastapi-service/app/config.py
@@ -7,11 +7,11 @@ ENV = os.getenv("ENV", "local")
 
 # Step 2: Load the correct .env file
 env_path = f".env.{ENV}"
-load_dotenv(dotenv_path=env_path)
+# load_dotenv(dotenv_path=env_path)
 
 # Step 3: Define settings class
 class Settings(BaseSettings):
-    ENV:str='local'
+    ENV:str=ENV
     DB_URL: str
     ALLOWED_ORIGINS: str
     PORT_FASTAPI: int = 8000  # Optional default
